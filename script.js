3a72b858ee494992a99753fa83c139bc3c1feaaf
Chase Gonzales <chasethedev@gmail.com>
2019-11-19 10:52:16 -0800

    small es6 and text changes

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
-var centerDiv = document.getElementById('centerDiv');
-//create play game button
-var playBtn = document.getElementById('playBtn');
-// create enter name button
-var enterBtn = document.createElement('button');
-// create first continue button
-var firstContinueBtn = document.createElement('button');
-// forest continue button
-var forestContinueBtn = document.createElement('button');
-// create next 1 button
-var next1Btn = document.createElement('button');
-var next2Btn = document.createElement('button');
-var next3Btn = document.createElement('button');
-// create input
-var input = document.getElementById('gameInput');
-
-var bodyWrap = document.getElementById('bodyWrap');
-// create player
-var player;
+// grab centerDiv ID 
+let centerDiv = document.getElementById('centerDiv');
+// grab bodywrap ID
+let bodyWrap = document.getElementById('bodyWrap');
 // create h2
-var h2 = document.createElement('h2');
+let h2 = document.createElement('h2');
 // add class to h2
 h2.classList.add('gameh2');
+// create player
+let player;
 
+//create play game button
+let playBtn = document.getElementById('playBtn');
 // first "click to play" button with event listener
 playBtn.addEventListener("click", function() {	
 	// set h2 text
@@ -38,9 +27,12 @@ playBtn.addEventListener("click", function() {
 	playBtn.parentNode.removeChild(playBtn);		
 });
 
+// create enter name button
+let enterBtn = document.createElement('button');
+// create input
+let input = document.getElementById('gameInput');
 //second button enter name event listener
 enterBtn.addEventListener("click", function (){
-
 	if (input.value.length > 0) { 
 		// assign input value to var player
 		player = document.getElementById('gameInput').value;
@@ -59,9 +51,15 @@ enterBtn.addEventListener("click", function (){
 	} 
  });
 
+// create first continue button
+let firstContinueBtn = document.createElement('button');
+// create next 1 button
+let next1Btn = document.createElement('button');
+// create next 2 button
+let next2Btn = document.createElement('button');
 // create top dialog box
-var someText = document.getElementById('someText');
-
+let someText = document.getElementById('someText');
+//first continue button event listener
 firstContinueBtn.addEventListener('click', function(){
 	bodyWrap.setAttribute('class', 'fade-out');
 	setTimeout(function(){
@@ -77,17 +75,18 @@ firstContinueBtn.addEventListener('click', function(){
 		// add classes
 		someText.setAttribute('class', 'narrator');
 		// inner text set
-		someText.innerText = 'The forst is cold. you have been on the road a long time. It has been days since you have even had a bite and the only water you can find is that from which you can get from eating snow. You can feel your energy draining. You have lost count of the days its been since you have seen another living soul. There is no home to go back to. Your only hope is to continue on and look for shelter' ;
+		someText.innerText = `The forst is cold. You have been on the road a long time. It has been days since you have even had a bite and the only water you can find is that which you can get from eating snow. You can feel your energy draining. You have lost count of the days it's been since you have seen another living soul. Every step is harder to take than the last but your only hope is to continue on in search for shelter and food.`;
 		// creates next 1 button
  		centerDiv.appendChild(next1Btn);
  		// add next text to next1 button
  		next1Btn.innerText = "next";
 	},4000)
+    //next 1 button event listener
 	next1Btn.addEventListener("click", function(){
 		//removes nextbtn1 button
 		next1Btn.parentNode.removeChild(next1Btn);
 		// change box inner text
-		someText.innerText = 'travel through trees';
+		someText.innerText = `It is best not to be visible out in the open. Take cover and travel through trees`;
 		//create next btn 2
 		centerDiv.appendChild(next2Btn);
 		// add next text to next2 button
@@ -95,27 +94,30 @@ firstContinueBtn.addEventListener('click', function(){
 	});
 });
 
+//next 2 button event listener
 next2Btn.addEventListener('click', function(){
 		//removes nextbtn2 button
 		next2Btn.parentNode.removeChild(next2Btn);
 		// changes id
 		document.getElementById('bodyWrapFrost1').id ='bodyWrapFrost2';
 		// changes some text
-		someText.innerText = 'You found a river! However it is frozen solid. It looks like there might be a small path on the other side of these trees';
+		someText.innerText = `You found a river! However it is frozen solid. This part of the forst is thick with dead trees and slippery icy bolders. It's quite hard to move through. There looks to be an easier flat path on the other side of these trees`;
 		//create next btn 3
 		centerDiv.appendChild(next3Btn);
 		// add next text to next3 button
- 		next3Btn.innerText = "next";
-		
+ 		next3Btn.innerText = "next";	
 })
 
+// create next 3 button
+let next3Btn = document.createElement('button');
+ //next 3 button event listener
 next3Btn.addEventListener('click', function(){
 		//removes nextbtn3 button
 		next3Btn.parentNode.removeChild(next3Btn);
 		// changes id
 		document.getElementById('bodyWrapFrost2').id ='bodyWrapFrost3';
 		// changes some text
-		someText.innerText = 'You found a river! However it is frozen solid. It looks like there might be a small path on the other side of these trees';
+		someText.innerText = `This looks much easier to travel through but you are not quite as hidden. Best be aware of your surroundings and travel with caution but the lack of food and sleep has made you a bit clumsy. You can feel your senses fading with each step.`;
 		// //create next btn 4
 		// centerDiv.appendChild(next4Btn);
 		// // add next text to next3 button
@@ -123,7 +125,8 @@ next3Btn.addEventListener('click', function(){
 		
 })
 
-
+// forest continue button
+// let forestContinueBtn = document.createElement('button');
 // setTimeout(function(){
 // 		// creates forest continue button
 // 		centerDiv.appendChild(forestContinueBtn);
